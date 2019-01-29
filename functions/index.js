9fb2ba7bf62a079053432258aafbf7d1c8d8f08e
Ken Mueller <kenmueller0@gmail.com>
2019-01-29 13:25:08 -0800

    functions fix

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,14 @@ admin.initializeApp();
 
 exports.transact = functions.database.ref('transactions/{uid}').onCreate((snapshot, context) => {
     const root = snapshot.ref.parent.parent
-    root.child(`users/${to}/balance`).once('value').then(snap => {
+    return root.child(`users/${to}/balance`).once('value').then(balanceSnapshot => {
 	    const from = context.params.uid
 	    const to = snapshot.val().to
-        const toBalance = snap.val() + amount
-        root.child(`transactions/${to}/${snapshot.key}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message})
-        root.child(`users/${from}/balance`).set(snapshot.val().balance)
-        root.child(`users/${to}/balance`).set(toBalance)
+        const toBalance = balanceSnapshot.val() + amount
+        return Promise.all([
+            root.child(`transactions/${to}/${snapshot.key}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message}),
+            root.child(`users/${from}/balance`).set(snapshot.val().balance),
+            root.child(`users/${to}/balance`).set(toBalance)
+        ])
     })
 });
