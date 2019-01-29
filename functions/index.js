fe2706dabf696dcb79f61617fae8705648b23d18
Ken Mueller <kenmueller0@gmail.com>
2019-01-29 11:53:16 -0800

    transact()

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.transact = functions.database.ref('transactions/{uid}').onCreate((snapshot, context) => {
+    const ref = snapshot.ref.parent.parent
+    ref.child(`users/${to}/balance`).once('value').then(snap => {
+	    const from = context.params.uid
+	    const to = snapshot.val().to
+        const toBalance = snap.val() + amount
+        ref.child(`transactions/${to}/${snapshot.key}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message})
+        ref.child(`users/${from}/balance`).set(snapshot.val().balance)
+        ref.child(`users/${to}/balance`).set(toBalance)
+    })
+});
