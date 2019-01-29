6332698481578f7e70ddd8321d7e461a65cd5230
Ken Mueller <kenmueller0@gmail.com>
2019-01-29 12:18:17 -0800

    root

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,13 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.transact = functions.database.ref('transactions/{uid}').onCreate((snapshot, context) => {
-    const ref = snapshot.ref.parent.parent
-    ref.child(`users/${to}/balance`).once('value').then(snap => {
+    const root = snapshot.ref.parent.parent
+    root.child(`users/${to}/balance`).once('value').then(snap => {
 	    const from = context.params.uid
 	    const to = snapshot.val().to
         const toBalance = snap.val() + amount
-        ref.child(`transactions/${to}/${snapshot.key}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message})
-        ref.child(`users/${from}/balance`).set(snapshot.val().balance)
-        ref.child(`users/${to}/balance`).set(toBalance)
+        root.child(`transactions/${to}/${snapshot.key}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message})
+        root.child(`users/${from}/balance`).set(snapshot.val().balance)
+        root.child(`users/${to}/balance`).set(toBalance)
     })
 });
