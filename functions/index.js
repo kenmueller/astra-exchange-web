192ed00f75e6b5db801f2c54b847dca4e5ca36c0
Ken Mueller <kenmueller0@gmail.com>
2019-01-29 17:28:32 -0800

    /2

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,8 @@ exports.transaction = functions.database.ref('transactions/{uid}/{transactionId}
     return root.child(`users/${to}/balance`).once('value').then(balanceSnapshot => {
         const from = context.params.uid
         const amount = snapshot.val().amount
-        const toBalance = balanceSnapshot.val() + amount
+        const toBalance = balanceSnapshot.val() + amount / 2
+        console.log('From, to, amount, toBalance', from, to, amount, toBalance)
         return Promise.all([
             root.child(`transactions/${to}/${context.params.transactionId}`).set({time: snapshot.val().time, from: from, to: to, amount: amount, balance: toBalance, message: snapshot.val().message}),
             root.child(`users/${from}/balance`).set(snapshot.val().balance),
