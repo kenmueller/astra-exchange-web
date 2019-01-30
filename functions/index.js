70133f6ee10c0746ca43f4e6d3417417e89173ea
Ken Mueller <kenmueller0@gmail.com>
2019-01-29 16:56:59 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,15 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.transact = functions.database.ref('transactions/{uid}/{transactionId}').onCreate((snapshot, context) => {
+exports.transaction = functions.database.ref('transactions/{uid}/{transactionId}').onCreate((snapshot, context) => {
     const root = snapshot.ref.parent.parent
     const to = snapshot.val().to
     return root.child(`users/${to}/balance`).once('value').then(balanceSnapshot => {
-	    const from = context.params.uid
+        const from = context.params.uid
+        const amount = snapshot.val().amount
         const toBalance = balanceSnapshot.val() + amount
         return Promise.all([
-            root.child(`transactions/${to}/${context.params.transactionId}`).set({time: snapshot.val().time, from: from, to: to, amount: snapshot.val().amount, balance: toBalance, message: snapshot.val().message}),
+            root.child(`transactions/${to}/${context.params.transactionId}`).set({time: snapshot.val().time, from: from, to: to, amount: amount, balance: toBalance, message: snapshot.val().message}),
             root.child(`users/${from}/balance`).set(snapshot.val().balance),
             root.child(`users/${to}/balance`).set(toBalance)
         ])
