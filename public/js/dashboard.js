b218c3e457ff0619a0123da7b25b331eddf641a4
Ken Mueller <kenmueller0@gmail.com>
2019-04-13 13:13:18 -0700

    base for modals and fixed contentloaded

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,24 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const auth = firebase.auth()
 	const db = firebase.database()
-	const actions = {
-		'Send Money': showSendModal,
-		'Create Invoice': showCreateInvoiceModal,
-		'Transaction History': showTransactionsModal,
-		'Invoices': showInvoicesModal,
-		'Your ID': showYourIdModal
-	}
 	let user
 
 	auth.onAuthStateChanged(function(newUser) {
 		user = newUser
 		if (user) {
-			db.ref(`users/${user.uid}`).on('value').then(function(snapshot) {
+			db.ref(`users/${user.uid}`).on('value', function(snapshot) {
 				const val = snapshot.val()
 				document.querySelectorAll('.user.name').forEach(element => element.innerHTML = `Hello, ${val.name}`)
 			})
-		} else {
-
 		}
 	})
+
+	function showSendModal() {
+		document.querySelectorAll('.modal.send').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideSendModal() {
+		document.querySelectorAll('.modal.send').forEach(element => element.classList.remove('is-active'))
+	}
+
+	function showCreateInvoiceModal() {
+		document.querySelectorAll('.modal.create-invoice').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideCreateInvoiceModal() {
+		document.querySelectorAll('.modal.create-invoice').forEach(element => element.classList.remove('is-active'))
+	}
+
+	function showTransactionsModal() {
+		document.querySelectorAll('.modal.transactions').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideTransactionsModal() {
+		document.querySelectorAll('.modal.transactions').forEach(element => element.classList.remove('is-active'))
+	}
+
+	function showInvoicesModal() {
+		document.querySelectorAll('.modal.invoices').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideInvoicesModal() {
+		document.querySelectorAll('.modal.invoices').forEach(element => element.classList.remove('is-active'))
+	}
+
+	function showYourIdModal() {
+		document.querySelectorAll('.modal.your-id').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideYourIdModal() {
+		document.querySelectorAll('.modal.your-id').forEach(element => element.classList.remove('is-active'))
+	}
+
+	document.querySelectorAll('.action.send').forEach(element => element.addEventListener('click', showSendModal))
+	document.querySelectorAll('.action.create-invoice').forEach(element => element.addEventListener('click', showCreateInvoiceModal))
+	document.querySelectorAll('.action.transactions').forEach(element => element.addEventListener('click', showTransactionsModal))
+	document.querySelectorAll('.action.invoices').forEach(element => element.addEventListener('click', showInvoicesModal))
+	document.querySelectorAll('.action.your-id').forEach(element => element.addEventListener('click', showYourIdModal))
 })
