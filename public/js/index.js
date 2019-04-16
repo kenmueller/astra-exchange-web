7e9474331eacb1f908e5450a539ca8f344df8bfd
Ken Mueller <kenmueller0@gmail.com>
2019-04-15 20:59:39 -0700

    automatically downloads app

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const auth = firebase.auth()
+	let user
 
-	auth.onAuthStateChanged(function(user) {
+	if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+		window.location.href='itms-services://?action=download-manifest&url=https://astra.exchange/manifest.plist'
+	}
+
+	auth.onAuthStateChanged(function(user_) {
+		user = user_
 		if (user) {
 			window.location.href = '/dashboard'
 		}
 	})
+
+	function dashboard() {
+		if (user) {
+			window.location.href = '/dashboard'
+		} else {
+			alert('You must be signed in to visit your dashboard')
+		}
+	}
+
+	document.querySelectorAll('.go-to-dashboard').forEach(element => element.addEventListener('click', dashboard))
 })
