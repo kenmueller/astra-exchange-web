c88899e2610687d1b17113726eb1e8a82b0cfdfe
Ken Mueller <kenmueller0@gmail.com>
2019-04-16 01:08:26 -0700

    working on https functions 1 & 2

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions')
-require('firebase-admin').initializeApp()
+const admin = require('firebase-admin')
+admin.initializeApp()
+const moment = require('moment')
+const db = admin.database()
 
 exports.tableOrderCreated = functions.database.ref('users/{uid}/bazaarTableOrders/{tableOrder}').onCreate((snapshot, context) => {
 	const root = snapshot.ref.parent.parent.parent.parent
@@ -82,3 +85,67 @@ exports.userCreated = functions.database.ref('users/{uid}').onCreate((snapshot, 
 exports.cardCreated = functions.database.ref('cards/{cardId}').onCreate((snapshot, context) =>
 	snapshot.ref.parent.parent.child(`users/${snapshot.val()}/cards/${context.params.cardId}`).set({ name: 'Debit Card', pin: (Math.floor(Math.random() * 10000) + 10000).toString().substring(1) })
 )
+
+exports.users = functions.https.onRequest((_req, res) =>
+	db.ref('users').once('value', snapshot => {
+		const val = snapshot.val()
+		return res.status(200).send(Object.keys(val).map(key => {
+			const user = val[key]
+			return { id: key, name: user.name, email: user.email, balance: user.balance }
+		}))
+	})
+)
+
+exports.transaction = functions.https.onRequest((req, res) => {
+	const pin = req.query.pin
+	if (pin) {
+		const from = req.query.from
+		if (from) {
+			const to = req.query.to
+			if (to) {
+				return db.ref(`users/${from}/balance`).once('value', fromSnapshot => {
+					if (fromSnapshot.exists()) {
+						const amount = parseInt(req.query.amount)
+						const balance = fromSnapshot.val()
+						if (amount <= balance) {
+							return db.ref(`users/${to}`).once('value', toSnapshot => {
+								if (toSnapshot.exists()) {
+									const message = req.query.message
+									return db.ref(`users/${from}/cards`).once('child_added', cardSnapshot => {
+										if (req.query.pin === cardSnapshot.val().pin) {
+											const dateList = moment().format('lll').split(' ')
+											dateList.splice(3, 0, '@')
+											return db.ref(`transactions/${from}`).push({ time: dateList.join(' '), from: from, to: to, amount: amount, balance: balance - amount, message: message ? message : '' }, error => {
+												if (error) {
+													return res.status(500).send(`Error creating transaction. ${error}`)
+												} else {
+													return res.status(200).send('Successfully created transaction')
+												}
+											})
+										} else {
+											return res.status(401).send(`Invalid pin for user ${from}`)
+										}
+									})
+								} else {
+									return res.status(404).send(`No user with ID ${to}`)
+								}
+							})
+						} else {
+							return res.status(403).send(`Insufficient balance`)
+						}
+					} else {
+						return res.status(404).send(`No user with ID ${from}`)
+					}
+				})
+			} else {
+				return res.status(400).send('Must specify the to ID')
+			}
+		} else {
+			return res.status(400).send('Must specify the from ID')
+		}
+	} else {
+		return res.status(400).send('Must specify the pin')
+	}
+})
+
+exports
