8efa90935fe81614514ac21be10121674934cb45
Ken Mueller <kenmueller0@gmail.com>
2019-04-16 17:33:38 -0700

    settings for documentation

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const auth = firebase.auth()
+	const db = firebase.database()
 	let user
 
 	if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
@@ -7,8 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	auth.onAuthStateChanged(function(user_) {
-		user = user_
+		if (user_) {
+			const id = user_.uid
+			db.ref(`users/${id}`).on('value', function(snapshot) {
+				const val = snapshot.val()
+				user = { id: id, name: val.name, email: val.email, balance: val.balance, independence: val.independence, card: null }
+				updateSettings()
+				db.ref(`users/${id}/cards`).on('child_added', function(cardSnapshot) {
+					const cardVal = cardSnapshot.val()
+					user.card = { id: cardSnapshot.key, name: cardVal.name, pin: cardVal.pin }
+					updateSettings()
+				})
+			})
+		}
 	})
+
+	function updateSettings() {
+		document.querySelectorAll('.settings.name').forEach(element => element.innerHTML = user.name)
+		document.querySelectorAll('.settings.email').forEach(element => element.innerHTML = user.email)
+		document.querySelectorAll('.settings.balance').forEach(element => element.innerHTML = user.balance)
+		document.querySelectorAll('.settings.independence').forEach(element => element.innerHTML = user.independence === 0 ? 'Pending' : user.independence)
+		if (user.card) {
+			document.querySelectorAll('.settings.pin').forEach(element => element.innerHTML = user.card.pin)
+		}
+	}
 
 	function dashboard() {
 		if (user) {
@@ -18,5 +41,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 
+	function showSettingsModal() {
+		document.querySelectorAll('.modal.settings').forEach(element => element.classList.add('is-active'))
+	}
+
+	function hideSettingsModal() {
+		document.querySelectorAll('.modal.settings').forEach(element => element.classList.remove('is-active'))
+	}
+
+	function resetPassword() {
+		auth.sendPasswordResetEmail(user.email)
+		alert(`Password reset email sent to ${user.email}`)
+	}
+
 	document.querySelectorAll('.go-to-dashboard').forEach(element => element.addEventListener('click', dashboard))
+	document.querySelectorAll('.action.settings').forEach(element => element.addEventListener('click', showSettingsModal))
+	document.querySelectorAll('.close-settings').forEach(element => element.addEventListener('click', hideSettingsModal))
+	document.querySelectorAll('.button.password-reset').forEach(element => element.addEventListener('click', resetPassword))
 })
