34e8e844317bf2d550fa50050f4dfccd5b53274e
Ken Mueller <kenmueller0@gmail.com>
2019-04-16 17:23:46 -0700

    go-to-dashboard

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+	const auth = firebase.auth()
+	let user
+
 	if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
 		window.location.href = 'itms-services://?action=download-manifest&url=https://astra.exchange/manifest.plist'
 	}
+
+	auth.onAuthStateChanged(function(user_) {
+		user = user_
+	})
+
+	function dashboard() {
+		if (user) {
+			window.location.href = '/dashboard'
+		} else {
+			alert('You must be signed in to visit your dashboard')
+		}
+	}
+
+	document.querySelectorAll('.go-to-dashboard').forEach(element => element.addEventListener('click', dashboard))
 })
