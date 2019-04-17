89016993c07828e0ae183c3c9d9c65bcaa9baa5e
Ken Mueller <kenmueller0@gmail.com>
2019-04-17 15:49:47 -0700

    functions module

---
+++ public/use/js/all.js
@@ -0,0 +1,94 @@
+exports.users = callback =>
+	fetch('https://us-central1-astra-exchange.cloudfunctions.net/users').then(response => {
+		if (response.readyState === 4)
+			return response.json().then(json =>
+				callback(json)
+			)
+	})
+
+exports.transact = (pin, from, to, amount, message, success, failure) =>
+    fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-astra-exchange.cloudfunctions.net/transact?pin=${pin}&from=${from}&to=${to}&amount=${amount}${message ? `&message=${message}` : ''}`).then(response => {
+        if (response.readyState === 4)
+            switch (response.status) {
+                case 200:
+                    success()
+                    break
+                case 400:
+                    failure(400, 'Invalid parameters')
+                    break
+                case 404:
+                    failure(404, 'Invalid user ID')
+                    break
+                case 403:
+                    failure(403, 'Insufficient balance')
+                    break
+                case 401:
+                    failure(401, 'Invalid pin')
+                    break
+                default:
+                    failure(500, 'Unknown error. Please try again')
+            }
+	})
+
+exports.userWithId = (id, pin, success, failure) =>
+	fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-astra-exchange.cloudfunctions.net/user?id=${id}${pin ? `&pin=${pin}` : ''}`).then(response => {
+		if (response.readyState === 4)
+			switch (response.status) {
+				case 200:
+					success()
+					break
+				case 400:
+					failure(400, 'Invalid parameters')
+					break
+				case 404:
+					failure(404, 'Invalid user ID')
+					break
+				case 401:
+					failure(401, 'Invalid pin')
+					break
+				default:
+					failure(500, 'Unknown error. Please try again')
+			}
+	})
+
+exports.userWithEmail = (email, pin, success, failure) =>
+	fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-astra-exchange.cloudfunctions.net/user?email=${email}${pin ? `&pin=${pin}` : ''}`).then(response => {
+		if (response.readyState === 4)
+			switch (response.status) {
+				case 200:
+					success()
+					break
+				case 400:
+					failure(400, 'Invalid parameters')
+					break
+				case 404:
+					failure(404, 'Invalid email')
+					break
+				case 401:
+					failure(401, 'Invalid pin')
+					break
+				default:
+					failure(500, 'Unknown error. Please try again')
+			}
+	})
+
+exports.transactions = (id, pin, success, failure) =>
+	fetch(`https://cors-anywhere.herokuapp.com/https://us-central1-astra-exchange.cloudfunctions.net/transactions?id=${id}&pin=${pin}`).then(response => {
+		if (response.readyState === 4)
+			switch (response.status) {
+				case 200:
+					success()
+					break
+				case 400:
+					failure(400, 'Invalid parameters')
+					break
+				case 404:
+					failure(404, 'Invalid user ID')
+					break
+				case 401:
+					failure(401, 'Invalid pin')
+					break
+				default:
+					failure(500, 'Unknown error. Please try again')
+			}
+	})
