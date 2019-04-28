c0981ff10691423d96111861917167c03f7c8db2
Ken Mueller <kenmueller0@gmail.com>
2019-04-28 00:54:41 -0700

    signs out properly in auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -58,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function signOut() {
 		auth.signOut().then(() => {
+			document.cookie = 'auth=; expires=Thu, 01 Jan 1970 00:00:00 GMT'
 			window.location.href = '/'
 		}, error => {
 			alert(error)
