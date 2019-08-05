00f91a778a1cb7b5166e73a056c16e28ed3fae70
Ken Mueller <kenmueller0@gmail.com>
2019-08-05 00:04:49 -0700

    no force unwrap

--- functions/src/App.ts
+++ functions/src/App.ts
@@ -17,50 +17,58 @@ const db = admin.database()
 
 app.get('/users/:slug', (req, res) =>
 	db.ref(`slugs/users/${req.params.slug}`).on('value', snapshot =>
-		db.ref(`users/${snapshot!.val()}`).on('value', userSnapshot => {
-			if (userSnapshot && userSnapshot.exists()) {
-				const val = userSnapshot.val()
-				res.status(200).render('user.html', {
-					user: {
-						id: userSnapshot.key,
-						name: val.name,
-						email: val.email,
-						balance: val.balance
-					}
-				})
-			} else
-				res.status(404).redirect('/users')
-		})
+		snapshot
+			? db.ref(`users/${snapshot.val()}`).on('value', userSnapshot => {
+				if (userSnapshot && userSnapshot.exists()) {
+					const val = userSnapshot.val()
+					res.status(200).render('user.html', {
+						user: {
+							id: userSnapshot.key,
+							name: val.name,
+							email: val.email,
+							balance: val.balance
+						}
+					})
+				} else
+					res.status(404).redirect('/users')
+			})
+			: res.sendStatus(500)
 	)
 )
 
 app.get('/companies/:slug', (req, res) =>
 	db.ref(`slugs/companies/${req.params.slug}`).on('value', snapshot =>
-		db.ref(`companies/${snapshot!.val()}`).on('value', companySnapshot => {
-			if (companySnapshot && companySnapshot.exists()) {
-				const val = companySnapshot.val()
-				const image = val.image ? val.image : '/images/astra.png'
-				return db.ref(`users/${val.owner}`).on('value', userSnapshot => {
-					const userVal = userSnapshot!.val()
-					res.status(200).render('company.html', {
-						company: {
-							id: companySnapshot.key,
-							name: val.name,
-							image,
-							description: val.description
-						},
-						user: {
-							id: userSnapshot!.key,
-							name: userVal.name,
-							email: userVal.email,
-							balance: userVal.balance
+		snapshot
+			? db.ref(`companies/${snapshot.val()}`).on('value', companySnapshot => {
+				if (companySnapshot && companySnapshot.exists()) {
+					const val = companySnapshot.val()
+					const image = val.image ? val.image : '/images/astra.png'
+					return db.ref(`users/${val.owner}`).on('value', userSnapshot => {
+						if (!userSnapshot) {
+							res.sendStatus(500)
+							return
 						}
+						const userVal = userSnapshot.val()
+						res.status(200).render('company.html', {
+							company: {
+								id: companySnapshot.key,
+								name: val.name,
+								image,
+								description: val.description
+							},
+							user: {
+								id: userSnapshot.key,
+								name: userVal.name,
+								email: userVal.email,
+								balance: userVal.balance
+							}
+						})
 					})
-				})
-			} else {
-				res.status(404).redirect('/companies')
-				return Promise.resolve()
-			}
-		})
+				} else {
+					res.status(404).redirect('/companies')
+					return Promise.resolve()
+				}
+			})
+			: res.sendStatus(500)
 	)
 )
