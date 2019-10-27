7348d952d600dc4816180175b1ec7cc6195a279e
Ken Mueller <kenmueller0@gmail.com>
2019-10-27 12:41:03 -0700

    using cors

--- functions/src/App.ts
+++ functions/src/App.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as express from 'express'
+import * as cors from 'cors'
 import { configure } from 'nunjucks'
 import { join } from 'path'
 
@@ -14,6 +15,8 @@ configure(join(__dirname, '../views'), {
 })
 
 const db = admin.database()
+
+app.use(cors())
 
 app.get('/users/:slug', (req, res) =>
 	db.ref(`slugs/users/${req.params.slug}`).on('value', snapshot =>
